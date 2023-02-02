190507ef61a9c3f43ad6ce90ab9e7042ca47227c
Nestor Jann Asag <nestorjannasag@gmail.com>
2023-02-02 09:25:12 +0800

    Updated the data grid

--- Maintenance_Form.Designer.cs
+++ Maintenance_Form.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             MetroFramework.Controls.MetroTabPage metroTab_OfficialsStaff;
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -55,8 +56,8 @@
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column8 = new System.Windows.Forms.DataGridViewImageColumn();
-            this.Column9 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.btn_Edit1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.bttn_Delete1 = new System.Windows.Forms.DataGridViewImageColumn();
             metroTab_OfficialsStaff = new MetroFramework.Controls.MetroTabPage();
             this.metroPanel1.SuspendLayout();
             this.metroPanel2.SuspendLayout();
@@ -270,7 +271,18 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI Variable Small", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
@@ -280,12 +292,13 @@
             this.Column5,
             this.Column6,
             this.Column7,
-            this.Column8,
-            this.Column9});
+            this.btn_Edit1,
+            this.bttn_Delete1});
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.GridColor = System.Drawing.Color.White;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.Size = new System.Drawing.Size(1058, 594);
             this.dataGridView1.TabIndex = 2;
             // 
@@ -296,43 +309,59 @@
             // 
             // Column2
             // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.Column2.HeaderText = "NAME";
             this.Column2.Name = "Column2";
+            this.Column2.Width = 91;
             // 
             // Column3
             // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.Column3.HeaderText = "CHAIRMANSHIP";
             this.Column3.Name = "Column3";
+            this.Column3.Width = 177;
             // 
             // Column4
             // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.Column4.HeaderText = "POSITION";
             this.Column4.Name = "Column4";
+            this.Column4.Width = 122;
             // 
             // Column5
             // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.Column5.HeaderText = "TERM START";
             this.Column5.Name = "Column5";
+            this.Column5.Width = 144;
             // 
             // Column6
             // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.Column6.HeaderText = "TERM END";
             this.Column6.Name = "Column6";
+            this.Column6.Width = 129;
             // 
             // Column7
             // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.Column7.HeaderText = "STATUS";
             this.Column7.Name = "Column7";
+            this.Column7.Width = 101;
             // 
-            // Column8
+            // btn_Edit1
             // 
-            this.Column8.HeaderText = "";
-            this.Column8.Name = "Column8";
+            this.btn_Edit1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.btn_Edit1.HeaderText = "";
+            this.btn_Edit1.Name = "btn_Edit1";
+            this.btn_Edit1.Width = 5;
             // 
-            // Column9
+            // bttn_Delete1
             // 
-            this.Column9.HeaderText = "";
-            this.Column9.Name = "Column9";
+            this.bttn_Delete1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.bttn_Delete1.HeaderText = "";
+            this.bttn_Delete1.Name = "bttn_Delete1";
+            this.bttn_Delete1.Width = 5;
             // 
             // Maintenance_Form
             // 
@@ -382,7 +411,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
-        private System.Windows.Forms.DataGridViewImageColumn Column8;
-        private System.Windows.Forms.DataGridViewImageColumn Column9;
+        private System.Windows.Forms.DataGridViewImageColumn btn_Edit1;
+        private System.Windows.Forms.DataGridViewImageColumn bttn_Delete1;
     }
 }
